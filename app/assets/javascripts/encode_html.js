3c9d1fdc8129ff25667e3659ee821c24fc7d7630
Le Duc Duy <me@duy.kr>
2013-03-27 18:46:03 +0900

    \n to <br>

--- app/assets/javascripts/encode_html.js
+++ app/assets/javascripts/encode_html.js
@@ -1,6 +1,6 @@
 function encodeHTMLSource() {
-    var encodeHTMLRules = { "&": "&#38;", "<": "&#60;", ">": "&#62;", '"': '&#34;', "'": '&#39;', "/": '&#47;' },
-        matchHTML = /&(?!#?\w+;)|<|>|"|'|\//g;
+    var encodeHTMLRules = { "&": "&#38;", "<": "&#60;", ">": "&#62;", '"': '&#34;', "'": '&#39;', "/": '&#47;', "\n": '<br>' },
+        matchHTML = /&(?!#?\w+;)|<|>|"|'|\/\n/g;
     return function() {
         return this ? this.replace(matchHTML, function(m) {return encodeHTMLRules[m] || m;}) : this;
     };
--- lib/sprockets-dotjs/version.rb
+++ lib/sprockets-dotjs/version.rb
@@ -1,5 +1,5 @@
 module Sprockets
   module Dotjs
-    VERSION = "2.0.3"
+    VERSION = "2.1.0"
   end
 end
