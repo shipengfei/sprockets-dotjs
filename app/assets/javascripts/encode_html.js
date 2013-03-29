56f63310916f567827ba78211a57e2d9954eb572
Le Duc Duy <me@duy.kr>
2013-03-30 03:58:06 +0900

    Fix <br>

--- app/assets/javascripts/encode_html.js
+++ app/assets/javascripts/encode_html.js
@@ -1,8 +1,8 @@
 function encodeHTMLSource() {
-    var encodeHTMLRules = { "&": "&#38;", "<": "&#60;", ">": "&#62;", '"': '&#34;', "'": '&#39;', "/": '&#47;', "\n": '<br>' },
-        matchHTML = /&(?!#?\w+;)|<|>|"|'|\/\n/g;
+    var encodeHTMLRules = { "&": "&#38;", "<": "&#60;", ">": "&#62;", '"': '&#34;', "'": '&#39;', "/": '&#47;'},
+        matchHTML = /&(?!#?\w+;)|<|>|"|'|\//g;
     return function() {
-        return this ? this.replace(matchHTML, function(m) {return encodeHTMLRules[m] || m;}) : this;
+        return this ? this.replace(matchHTML, function(m) {return encodeHTMLRules[m] || m; }).replace(/\n/,'<br>') : this;
     };
 }
 String.prototype.encodeHTML = encodeHTMLSource();
--- lib/sprockets-dotjs/version.rb
+++ lib/sprockets-dotjs/version.rb
@@ -1,5 +1,5 @@
 module Sprockets
   module Dotjs
-    VERSION = "2.1.0"
+    VERSION = "2.1.1"
   end
 end
